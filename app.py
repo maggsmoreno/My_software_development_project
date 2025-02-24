357002967425f1b09ad3dcfe316fbf164826c43b
maggsmoreno <Magdalena.Escobar24@gmail.com>
2025-02-24 09:09:40 -0600

    Saving local changes before rebase

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ df = pd.read_csv("notebooks/vehicles_us.csv")
 st.write("### Preview of Dataset")
 st.dataframe(df.head())  
 
+st.title("Exploratory Data Analysis")
+
+show_data = st.checkbox("Show DataFrame")
+
+if show_data:
+    st.write("Here is my dataset:")
+    st.dataframe(df)
+else:
+    st.write("Check the box above to display the data.")
+
+g = px.histogram(df, x="price", nbins=50, title="Distribution of Vehicle Prices")
+st.plotly_chart(g)
